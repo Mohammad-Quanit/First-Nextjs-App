ce73b3fa40034331fca9836f521c4556fbc4cf97
mohammad-quanit <muhammadquanit@gmail.com>
2021-09-01 00:49:59 +0500

    add: dynmaic SSG pages

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,44 +1,41 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { NextRouter, useRouter } from 'next/router';
 
-const User: NextPage = ({ user }: any) => {
-  console.log(user)
-  const router: NextRouter = useRouter();
-  const { id } = router.query;
+import { IUser } from '../users';
 
+const User: NextPage = ({ user }: any) => {
+  // const router: NextRouter = useRouter();
+  // const { id } = router.query;
   return (
     <div>
-      <div>Name: {id}</div>
+      <h1>User {user.id} Details</h1>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
     </div>
   );
 };
 
-// This function gets called at build time
 export const getStaticPaths: GetStaticPaths = async () => {
   // Call an external API endpoint to get users
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res: Response = await fetch(
+    'https://jsonplaceholder.typicode.com/users'
+  );
   const users = await res.json();
-
   // Get the paths we want to pre-render based on users
-  const paths = users.map((user) => ({
-    user: { id: user.id },
+  const paths = users.map((user: IUser) => ({
+    params: { id: user.id.toString() },
   }));
-  console.log(paths);
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
 };
 
-// This also gets called at build time
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  // params contains the user `id`.
-  // If the route is like /users/1, then user.id is 1
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/users/${params?.id}`
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id: string = context?.params?.id as string;
+  const res: Response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${id}`
   );
-  const user = await res.json();
-
-  // Pass user data to the page via props
+  const user: IUser = await res.json();
   return { props: { user } };
 };
 
--- pages/users.tsx
+++ pages/users.tsx
@@ -2,8 +2,7 @@ import type { GetStaticProps, NextPage } from 'next';
 import Link from 'next/link';
 
 import React from 'react';
-
-interface IUser {
+export interface IUser {
   id: number;
   name: string;
   username: string;
@@ -23,16 +22,14 @@ interface IUser {
 const Users: NextPage = ({ users }: any) => {
   return (
     <main>
-      <h1>Fetching Users</h1>
+      <h1>Fetched Users</h1>
       <ul>
         {users.map((user: IUser) => (
-          <Link
-            key={user.id}
-            href={`/user/${encodeURIComponent(user.id)}`}
-            passHref
-          >
-            <li>{user.name}</li>
-          </Link>
+          <div key={user.id} style={{ padding: '10px' }}>
+            <Link href={`/user/${encodeURIComponent(user.id)}`} passHref>
+              <li>{user.name}</li>
+            </Link>
+          </div>
         ))}
       </ul>
     </main>
@@ -40,7 +37,7 @@ const Users: NextPage = ({ users }: any) => {
 };
 
 // This function gets called at build time
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async () => {
   // Call an external API endpoint to get users
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const users = await res.json();
@@ -53,6 +50,5 @@ export const getStaticProps: GetStaticProps = async (context) => {
     },
   };
 };
-
 
 export default Users;
